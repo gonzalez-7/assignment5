b0d77e79dc07c8b604192c2c47b1cfc9ebeda3c8
gonzalez-7 <20agonzalezp@gmail.com>
2024-11-10 17:06:16 -0800

    Add sum function to calculate the total of an array of numbers

--- script.js
+++ script.js
@@ -15,8 +15,18 @@ console.log(range(1, 10)); // outputs [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
 
 // 2. Next, write a sum function that takes an array of numbers and returns the sum of
 // these numbers. Run the example program and see whether it does indeed return 55.
+
+function sum(numbers) {
+    let total = 0;
+    for (let number of numbers) {
+        total += number;
+    }
+    return total;
+}
+
 // Example output:
-// console.log(sum(range(1, 10))); // returns 55
+
+console.log(sum(range(1, 10))); // returns 55
 
 // 3. Arrays have a reverse method that changes the array by inverting the order in which
 // its elements appear. For this exercise, write two functions, reverseArray and
